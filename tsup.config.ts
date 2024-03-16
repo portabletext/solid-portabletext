885f8eabb2bcccda44f86fb2541e01a1b08f1739
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-03-16 17:46:56 +0100

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,36 @@
-import { defineConfig } from 'tsup-preset-solid'
+import { defineConfig } from 'tsup'
+import * as preset from 'tsup-preset-solid'
 
-export default defineConfig(
-  {
-    entry: 'src/index.tsx',
-    devEntry: true,
-  },
-  {
-    // Enable this to write export conditions to package.json
-    // writePackageJson: true,
-    dropConsole: true,
+const preset_options: preset.PresetOptions = {
+    // array or single object
+    entries: [
+        {
+            // entries with '.tsx' extension will have `solid` export condition generated
+            entry: 'src/index.tsx',
+            dev_entry: true,
+            // set `true` or pass a specific path to generate a server-only entry
+            server_entry: true,
+        },
+    ],
+    drop_console: true,
     cjs: true,
-  },
-)
+}
+
+export default defineConfig(config => {
+    const watching = !!config.watch
+
+    const parsed_options = preset.parsePresetOptions(preset_options, watching)
+
+    if (!watching) {
+        const package_fields = preset.generatePackageExports(parsed_options)
+
+        console.log(`package.json: \n\n${JSON.stringify(package_fields, null, 2)}\n\n`)
+
+        /*
+            will update ./package.json with the correct export fields
+        */
+        preset.writePackageJson(package_fields)
+    }
+
+    return preset.generateTsupOptions(parsed_options)
+})
